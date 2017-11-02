59626167c037dbdd03d3f0d1e0fe64faf0dd85e6
User <nrina95@gmail.com>
2017-11-02 11:41:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/firebasedatabasecrud/Artist.java
@@ -0,0 +1,30 @@
+package com.example.user.firebasedatabasecrud;
+
+/**
+ * Created by User on 2/11/2017.
+ */
+
+public class Artist
+{
+    String artistId;
+    String artistName;
+
+    public Artist()
+    {
+
+    }
+
+    public Artist(String artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+}
+
